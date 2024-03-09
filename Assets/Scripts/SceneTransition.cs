f8acaad3e863bab3398f2db493fcabda7d5d0f54
voronov <114691383+voronov-nikita@users.noreply.github.com>
2024-03-09 13:12:46 +0300

    add structure

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,20 @@
+// Код для пере
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
